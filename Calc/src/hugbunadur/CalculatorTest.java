762b5c4dcaf0ebf353fc079c7cd6f8ecf93c1e4e
jonj12 <jonj12@ru.is>
2014-10-13 11:56:21 +0000

    jar file fyrir Junit

--- Calc/src/hugbunadur/CalculatorTest.java
+++ Calc/src/hugbunadur/CalculatorTest.java
@@ -1,5 +1,19 @@
 package hugbunadur;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class CalculatorTest {
 
+	public class CalcTest{
+
+		public void main(String[] args)
+		{
+			org.junit.runner.JUnitCore.main("CalcTest");
+		}
+
+		@Test
+		public void test(){
+			assertEquals(0, Calculator.add(""));
+		}
+	}
 }
